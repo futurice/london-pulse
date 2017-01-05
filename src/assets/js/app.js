627bbbc56c0858ffc5ee3d3a77cf9e8da67f29a8
Marie Lu Vinh <mluvinh@gmail.com>
2017-01-05 23:09:03 +0000

    Add CSV parsing with papa parse

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,11 +1,30 @@
 $(document).foundation();
-var url = 'assets/data/responses.csv';
 
-function getData() {
-    return $.get(url).then(function(results){
-        return results;
+const dataFiles = [
+    "assets/data/pulse_data.csv",
+    "assets/data/answers_september.csv",
+    "assets/data/answers_october.csv",
+    "assets/data/answers_november.csv"
+];
+
+function loadCSV(url) {
+    return $.get(url).then(function(data){
+        return Papa.parse(data, {
+            header: true,
+            dynamicTyping: true
+        });
     });
 };
+
+const dataPromise = Promise.all(
+    dataFiles.map(loadCSV)
+).then(function(results){
+    return results.map(function(result) {
+        return result.data;
+    }).reduce(function(a,b) {
+        return a.concat(b);
+    });
+});
 
 $(document).ready(function() {
     getData().then(function(results){
